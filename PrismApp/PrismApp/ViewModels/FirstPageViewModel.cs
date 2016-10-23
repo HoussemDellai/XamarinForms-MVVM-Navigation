712741362f1a552e6ac245cf94641b4f095f60df
Houssem <Houssem@DESKTOP-3R1JT8R>
2016-10-23 14:40:14 +0100

    First commit.

--- PrismApp/PrismApp/ViewModels/FirstPageViewModel.cs
+++ PrismApp/PrismApp/ViewModels/FirstPageViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
